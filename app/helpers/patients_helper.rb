f73085429e1d010fdb1b8be7b26a1d8c2f6d4117
TheLover_Z <sethverlo5@gmail.com>
2014-04-24 15:02:13 +0800

    patient_statuses moved into helper.

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,3 +1,10 @@
 module PatientsHelper
 
+  def patient_statuses
+    [[t("initial", :scope => :status), 'initial'],
+      [t("referred", :scope => :status), 'referred'],
+      [t("treatment", :scope => :status), 'treatment'],
+      [t("closed", :scope => :status), 'closed']]
+  end
+
 end
